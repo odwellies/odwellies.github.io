4b99054f58a72c0cdc033c071290366bac1a6779
schuylerwilliams <sky@Sky-MacBook-Air.local>
2018-05-22 17:12:29 -0500

    INIT

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+var OpenDoorWellness = (function () {
+
+	var thisIsAVariableOrFunction = "true";
+	
+	return {
+		thisIsAVariableOrFunction: thisIsAVariableOrFunction,
+	};
+
+})();
+
+
+$(document).ready(function(){
+	console.log(OpenDoorWellness.thisIsAVariableOrFunction);
+});
