0c8ca0da7ac56521a366dd7f6a5fc6178e2b4aaf
schuylerwilliams <skywilliams@gmail.com>
2018-07-11 12:07:15 -0500

    added owl carousel

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ var OpenDoorWellness = (function () {
 
 	var thisIsAVariableOrFunction = "true";
 	
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		items:1
+	});
+	
+	console.log('test');
+	
 	return {
 		thisIsAVariableOrFunction: thisIsAVariableOrFunction,
 	};
